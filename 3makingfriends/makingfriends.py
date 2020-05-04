9dc3539ba5b2fa308ff487e27da9585458a9e9a8
Hugo Mattsson <hu5174ma-s@student.lu.se>
2020-05-04 20:18:02 +0200

    Done and optimized.

--- 3makingfriends/makingfriends.py
+++ 3makingfriends/makingfriends.py
@@ -3,80 +3,55 @@ from collections import deque
 from collections import defaultdict
 
 LINE = sys.stdin.readline().split(' ')
-# N: int = int(LINE[0])
+N: int = int(LINE[0])
 M: int = int(LINE[1])
-parent = defaultdict(int)
+parent = [i for i in range(0,N+1)]
+# rank = [0]*(N+1)
 
 def main():
     edges = buildEdges()
-    # print(edges)
+    #print(edges)
     print(kruskal(edges))
-    #print(parent)
+
 
 def kruskal(edges):
     sum = 0
-    # ((u,v),w) = edges.popleft()
-    # parent[u] = v
-    # parent[v] = 0
-    
-    # while(len(edges) != 0):
-    #     ((u,v),w) = edges.popleft()
-    #     parentU = find(u)
-    #     parentV = find(v)
-
-    #     # if (parentU, parentV) == (u, v):
-    #     #     parent[v] = u
-    #     #     parent[u] = 0
-    #     #     sum += w
-    #     # elif parentU != u and parentV == v:
-    #     #     parent[v] = u
-    #     #     sum += w
-    #     # elif parentV != v and parentU == u:
-    #     #     parent[u] = v
-    #     #     sum += w
-    #     # elif 
-
-    #     if parentU != parentV:
-    #         #parent[u] = parentV
-    #         # if (parent[u], parent[v]) != (0,0):
-    #         if parentV == v:
-    #             parent[v] = 0
-    #         parent[parentU] = parentV
-    #         sum += w
 
     while(len(edges) != 0):
-        ((u,v),w) = edges.popleft()
+        u,v,w = edges.popleft()
         parentU = find(u)
         parentV = find(v)
+        # rankU = rank[u]
+        # rankV = rank[v]
 
         if parentU != parentV:
-            if (parentU, parentV) == (u, v):
-                parent[u] = 0
-                parent[v] = parentU
+            # if rankU > rankV:
+            #     parent[parentV] = parentU
+            # elif rankU < rankV:
+            #     parent[parentU] = parentV
+            # else:
+            #     parent[parentV] = parentU
+            #     rank[u] += 1
             parent[parentV] = parentU
             sum += w
 
     return sum
 
-
-
 def find(node):
-    p = parent[node]
-    if p == 0:
-        return node
-    else:
-        return find(p)
+    if parent[node] != node:
+        parent[node] = find(parent[node])
+    return parent[node]
 
 def buildEdges():
     edges = []
     for _ in range(M):
         edges.append(createEdge())
     
-    return deque(sorted(edges, key=lambda edge: edge[1]))
+    return deque(sorted(edges, key=lambda edge: edge[2]))
 
 def createEdge():
-    edgeTuple = lambda lst: ((lst[0], lst[1]), lst[2])
-    return edgeTuple(list(map(int, sys.stdin.readline().split(' '))))
+    #edgeTuple = lambda lst: ((lst[0], lst[1]), lst[2]) # [x,y,z] -> ((x,y),z)
+    return list(map(int, sys.stdin.readline().split(' ')))
 
 if __name__ == '__main__':
     main()
